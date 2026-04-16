ed0907a498113de19a7f0293cd610914d18cf6d4
SaiOhmSaiePaine <saiohmsaiepaine@my.jcu.edu.au>
2026-04-16 22:13:10 +0700

    The web app is ready

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 app = Flask(__name__)
 ## Load the model
-radomforestmodel = pickle.load(open('randomforest.pkl', 'rb'))
+randomforestmodel = pickle.load(open('randomforest.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 @app.route('/')
 def home():
@@ -19,9 +19,17 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
     new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    output = radomforestmodel.predict(new_data)
+    output = randomforestmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = randomforestmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text='The predicted price of the house is ${:,.2f}'.format(output))
 
 
 if __name__ == "__main__":
